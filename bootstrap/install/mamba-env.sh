09638c172e460c97d2d11503f7c0032261e7892a
Kolen Cheung <christian.kolen@gmail.com>
2026-04-16 20:27:20 +0100

    make all

--- bootstrap/install/mamba-env.sh
+++ bootstrap/install/mamba-env.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 __OPT_ROOT="${__OPT_ROOT:-"${HOME}/.local"}"
 MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:-"${HOME}/.miniforge3"}"
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
-ZDOTDIR="${ZDOTDIR:-"${XDG_CONFIG_HOME}/zsh"}"
 print_double_line() {
     echo '================================================================================'
 }
