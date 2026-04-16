ee82c3e38b985a26a15847069786d7b628e02702
Kolen Cheung <christian.kolen@gmail.com>
2026-04-16 20:51:24 +0100

    add dotfiles_install

--- bootstrap/install/src/bin/bootstrap.sh
+++ bootstrap/install/src/bin/bootstrap.sh
@@ -5,11 +5,8 @@ set -eo pipefail
 __MAMBA_ENV_DOWNLOAD=1
 
 source ../lib/util/git.sh
+source ../lib/dotfiles.sh
 
-# shellcheck disable=SC1090
-. ~/.zshenv || true
-# shellcheck disable=SC1090
-. ~/.zshrc || true
 # this must be after sourcing dotfiles
 source ../state/env.sh
 source ../lib/util/helpers.sh
@@ -21,18 +18,24 @@ source ../lib/mamba-env.sh
 main() {
 
     print_double_line
+    echo 'Installing dotfiles'
+    dotfiles_install
+    # shellcheck disable=SC1090
+    . ~/.bashrc || true
+
+    print_double_line
     echo 'Installing VSCode CLI'
     code_install
     print_double_line
     echo "Installing mamba to ${MAMBA_ROOT_PREFIX}"
     mamba_install
+    # shellcheck disable=SC1090
+    . ~/.bashrc || true
+
     print_double_line
     echo 'Installing system environment via mamba'
     mamba_env_install
     print_double_line
-
-    # shellcheck disable=SC1090
-    . ~/.zshrc || true
 
     print_double_line
     echo 'Generating SSH key and login to GitHub'
