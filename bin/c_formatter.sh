6ca56cfcb4507af0b838c192cd705f231284bef3
Kolen Cheung <christian.kolen@gmail.com>
2026-04-18 16:21:21 +0000

    add bin/c_formatter.sh

---
+++ bin/c_formatter.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+find . -type f \( \
+    -iname "*.c" -o \
+    -iname "*.cpp" -o \
+    -iname "*.h" -o \
+    -iname "*.cu" -o \
+    -iname "*.upc" \
+    \) \
+    -exec clang-format -i -style=WebKit {} \+
